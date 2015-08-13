32a301a599fe95602b5813ff099ae3f8662c45d6
Mark Pundsack <mp@heroku.com>
2015-08-13 17:25:06 -0500

    Fix test URL nock

--- test/commands/pipelines/info.js
+++ test/commands/pipelines/info.js
@@ -11,11 +11,11 @@ describe('pipelines:info', function () {
 
   it('displays the pipeline info and apps', function () {
     let self     = this;
-    let pipeline = {name: 'example', id: '0123'};
+    let pipelines = [{name: 'example', id: '0123'}];
     let apps     = [{name: 'example-staging', coupling: {stage: 'staging'}, pipeline: pipeline}, {name: 'example', coupling: {stage: 'production'}, pipeline: pipeline}, {name: 'example-admin', coupling: {stage: 'production'}, pipeline: pipeline}];
 
     nock('https://api.heroku.com')
-    .get('/pipelines/example')
+    .get('pipelines?eq[name]=example')
     .reply(200, pipeline);
 
     nock('https://api.heroku.com')
