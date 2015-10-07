7a5df532dd764dc4ac384e0037fb45ac7c0e2f17
Hrafn Eiríksson <hrafne@gmail.com>
2015-10-07 18:34:48 +0000

    Moving helper functions to the global scope.

--- commands/pipelines/diff.js
+++ commands/pipelines/diff.js
@@ -11,6 +11,64 @@ const PROMOTION_ORDER = ['development', 'staging', 'production'];
 const V3_HEADER = 'application/vnd.heroku+json; version=3';
 const PIPELINES_HEADER = V3_HEADER + '.pipelines';
 
+// Helper functions
+
+function kolkrabbiRequest(url, token) {
+  return request({
+    method: 'GET',
+    url: url,
+    headers: {
+      authorization: 'Bearer ' + token
+    },
+    json: true
+  }).spread(function (res, body) {
+    if (res.statusCode < 200 || res.statusCode >= 300) {
+      throw new Error('failed to fetch diff because of an internal server error...');
+    }
+    return body;
+  });
+}
+
+function *getLatestCommitHash(heroku, appName, appId) {
+  const release = yield heroku.request({
+    method: 'GET',
+    path: `/apps/${appId}/releases`,
+    headers: { 'Accept': V3_HEADER, 'Range': 'version ..; order=desc,max=1' },
+    partial: true
+  });
+  const slug = yield heroku.request({
+    method: 'GET',
+    path: `/apps/${appId}/slugs/${release[0].slug.id}`,
+    headers: { 'Accept': V3_HEADER }
+  });
+  return { name: appName, hash: slug.commit };
+}
+
+function *diff(sourceApp, downstreamApp, repo, githubToken, herokuUserAgent) {
+  if (sourceApp.hash === downstreamApp.hash) {
+    console.log(`\neverything is up to date between ${sourceApp.name} and ${downstreamApp.name}`);
+    return;
+  }
+  const diff = yield request({
+    url: `https://api.github.com/repos/${repo}/compare/${downstreamApp.hash}...${sourceApp.hash}`,
+    headers: {
+      authorization: 'token ' + githubToken,
+      'user-agent': herokuUserAgent,
+    },
+    json: true
+  }).get(1);
+
+  console.log(`\n${sourceApp.name} is ahead of ${downstreamApp.name} by ${diff.ahead_by} commit${diff.ahead_by === 1 ? '' : 's'}:`);
+  for (let i = diff.commits.length - 1; i >= 0; i--) {
+    let commit = diff.commits[i];
+    let abbreviatedHash = commit.sha.substring(0, 7);
+    let authoredDate = commit.commit.author.date;
+    let authorName = commit.commit.author.name;
+    let message = commit.commit.message.split('\n')[0];
+    console.log(`  ${abbreviatedHash}  ${authoredDate}  ${message} (${authorName})`);
+  }
+}
+
 module.exports = {
   topic: 'pipelines',
   command: 'diff',
@@ -19,64 +77,6 @@ module.exports = {
   needsAuth: true,
   needsApp: true,
   run: cli.command(function* (context, heroku) {
-
-    function kolkrabbiRequest(url, token) {
-      return request({
-        method: 'GET',
-        url: url,
-        headers: {
-          authorization: 'Bearer ' + token
-        },
-        json: true
-      }).spread(function (res, body) {
-        if (res.statusCode < 200 || res.statusCode >= 300) {
-          throw new Error('failed to fetch diff because of an internal server error...');
-        }
-        return body;
-      });
-    }
-
-    // TODO: Move something else.
-    function *getLatestCommitHash(appName, appId) {
-      const release = yield heroku.request({
-        method: 'GET',
-        path: `/apps/${appId}/releases`,
-        headers: { 'Accept': V3_HEADER, 'Range': 'version ..; order=desc,max=1' },
-        partial: true
-      });
-      const slug = yield heroku.request({
-        method: 'GET',
-        path: `/apps/${appId}/slugs/${release[0].slug.id}`,
-        headers: { 'Accept': V3_HEADER }
-      });
-      return { name: appName, hash: slug.commit };
-    }
-
-    function *diff(sourceApp, downstreamApp, repo, githubToken) {
-      if (sourceApp.hash === downstreamApp.hash) {
-        console.log(`\neverything is up to date between ${sourceApp.name} and ${downstreamApp.name}`);
-        return;
-      }
-      const diff = yield request({
-        url: `https://api.github.com/repos/${repo}/compare/${downstreamApp.hash}...${sourceApp.hash}`,
-        headers: {
-          authorization: 'token ' + githubToken,
-          'user-agent': heroku.options.userAgent,
-        },
-        json: true
-      }).get(1);
-
-      console.log(`\n${sourceApp.name} is ahead of ${downstreamApp.name} by ${diff.ahead_by} commit${diff.ahead_by === 1 ? '' : 's'}:`);
-      for (let i = diff.commits.length - 1; i >= 0; i--) {
-        let commit = diff.commits[i];
-        let abbreviatedHash = commit.sha.substring(0, 7);
-        let authoredDate = commit.commit.author.date;
-        let authorName = commit.commit.author.name;
-        let message = commit.commit.message.split('\n')[0];
-        console.log(`  ${abbreviatedHash}  ${authoredDate}  ${message} (${authorName})`);
-      }
-    }
-
     const targetAppName = context.app;
     const coupling = yield heroku.request({
       method: 'GET',
@@ -106,11 +106,13 @@ module.exports = {
     }
 
     // Fetch the hash of the latest release for {target, downstream[0], .., downstream[n]} apps.
-    const wrappedGetLatestCommitHash = co.wrap(getLatestCommitHash);
+    const wrappedGetLatestCommitHash = co.wrap(_.partial(getLatestCommitHash, heroku));
     const targetHash = yield cli.action(`Fetching latest app release for target app`,
       wrappedGetLatestCommitHash(targetAppName, targetAppId));
     const downstreamHashes = yield cli.action(`Fetching latest app releases for downstream apps`,
-        bluebird.all(downstreamApps.map(app => wrappedGetLatestCommitHash(app.name, app.id))));
+        bluebird.all(downstreamApps.map(function (app) {
+          return wrappedGetLatestCommitHash(app.name, app.id)
+        })));
 
     // Try to refrain from doing any GitHub/kolkrabbi API requests if none of the downstream
     // hashes differ from the target hash.
@@ -126,7 +128,8 @@ module.exports = {
       `https://kolkrabbi.heroku.com/apps/${coupling.app.id}/github`, heroku.options.token);
 
     for (const downstreamHash of downstreamHashes) {
-      yield diff(targetHash, downstreamHash, githubApp.repo, githubAccount.github.token);
+      yield diff(targetHash, downstreamHash,
+        githubApp.repo, githubAccount.github.token, heroku.options.userAgent);
     }
   })
 };
