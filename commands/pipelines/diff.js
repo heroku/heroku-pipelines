db017d15b77c440b22c5b4fab304da326b33ce07
Hrafn Eiríksson <hrafne@gmail.com>
2015-10-07 10:21:16 +0000

    Fetching the diff from GitHub

--- commands/pipelines/diff.js
+++ commands/pipelines/diff.js
@@ -4,6 +4,7 @@ let cli          = require('heroku-cli-util');
 let disambiguate = require('../../lib/disambiguate');
 let co           = require('co');
 var bluebird     = require('bluebird');
+var request      = bluebird.promisify(require('request'));
 
 const PROMOTION_ORDER = ['development', 'staging', 'production'];
 const V3_HEADER = 'application/vnd.heroku+json; version=3';
@@ -17,6 +18,22 @@ module.exports = {
   needsAuth: true,
   needsApp: true,
   run: cli.command(function* (context, heroku) {
+
+    function kolkrabbiRequest(url, token) {
+      return request({
+        method: 'GET',
+        url: url,
+        headers: {
+          authorization: 'Bearer ' + token
+        },
+        json: true
+      }).spread(function (res, body) {
+        if (res.statusCode < 200 || res.statusCode >= 300) {
+          throw new Error('failed to fetch diff because of an internal server error...');
+        }
+        return body;
+      });
+    }
 
     // TODO: Move something else.
     function *getLatestCommitHash(appId) {
@@ -66,10 +83,39 @@ module.exports = {
     // TODO: What if downstreamApps.length > 1?
 
     const wrappedGetLatestCommitHash = co.wrap(getLatestCommitHash);
-    const commitHashes = yield cli.action(`Waiting for stuff?`, bluebird.all([
+    const hashes = yield cli.action(`Fetching commit data`, bluebird.all([
       wrappedGetLatestCommitHash(downstreamApps[0].id),
       wrappedGetLatestCommitHash(app)
     ]));
-    console.log(commitHashes);
+
+    if (hashes[0] === hashes[1]) {
+      console.log(`everything is up to date`);
+      return;
+    }
+
+    const githubAccount = yield kolkrabbiRequest(
+      `https://kolkrabbi.heroku.com/account/github/token`, heroku.options.token);
+    const githubApp = yield kolkrabbiRequest(
+      `https://kolkrabbi.heroku.com/apps/${coupling.app.id}/github`, heroku.options.token);
+
+    const diff = yield cli.action(`Fetching diff from GitHub`, request({
+      url: `https://api.github.com/repos/${githubApp.repo}/compare/${hashes[0]}...${hashes[1]}`,
+      headers: {
+        authorization: 'token ' + githubAccount.github.token,
+        'user-agent': heroku.options.userAgent,
+      },
+      json: true
+    }).get(1));
+
+    console.log(`\n${app} is ahead by ${diff.ahead_by} commit${diff.ahead_by === 1 ? '' : 's'}`);
+    for (let i = 0; i < diff.commits.length; i++) {
+      let commit = diff.commits[i];
+      let abbreviatedHash = commit.sha.substring(0, 8);
+      let authoredDate = commit.commit.author.date;
+      let authorName = commit.commit.author.name;
+      let message = commit.commit.message;
+      console.log(`  ${abbreviatedHash}  ${authoredDate}  ${message} (${authorName})`);
+    }
+
   })
 };
