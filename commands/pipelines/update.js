b9892f5a1dc06a6f17cff7a9870314f861b164bb
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2016-01-28 14:17:10 +0100

    Extract pipelines api module

--- commands/pipelines/update.js
+++ commands/pipelines/update.js
@@ -1,31 +1,7 @@
 'use strict';
 
 let cli = require('heroku-cli-util');
-
-const V3_HEADER = 'application/vnd.heroku+json; version=3';
-const PIPELINES_HEADER = V3_HEADER + '.pipelines';
-
-function getCoupling(heroku, app) {
-  return heroku.request({
-    method: 'GET',
-    path: `/apps/${app}/pipeline-couplings`,
-    headers: { 'Accept': PIPELINES_HEADER }
-  });
-}
-
-function patchCoupling(heroku, id, stage) {
-  return heroku.request({
-    method: 'PATCH',
-    path: `/pipeline-couplings/${id}`,
-    body: {stage: stage},
-    headers: { 'Accept': PIPELINES_HEADER }
-  });
-}
-
-function updateCoupling(heroku, app, stage) {
-  return getCoupling(heroku, app)
-           .then(coupling => patchCoupling(heroku, coupling.id, stage));
-}
+const updateCoupling = require('../../lib/api').updateCoupling;
 
 module.exports = {
   topic: 'pipelines',
