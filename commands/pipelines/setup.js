3ab3d4303d7f33a1f44a0a9242646c25c1a94a92
raulb <ra.barroso@gmail.com>
2017-09-05 12:17:35 +0200

    Use old version of setup to test CI

--- commands/pipelines/setup.js
+++ commands/pipelines/setup.js
@@ -22,7 +22,7 @@ const DEFAULT_SETTINGS = {
   }
 }
 
-function validate ({name, repo}) {
+function validate ({ name, repo }) {
   const errors = []
   const [nameIsValid, nameMsg] = validateName(name || '')
   const [repoIsValid, repoMsg] = validateRepo(repo || '')
@@ -35,7 +35,7 @@ function validate ({name, repo}) {
 
 function validateName (name) {
   const isValid = name.length >= PIPELINE_MIN_LENGTH &&
-    name.length <= PIPELINE_MAX_LENGTH
+                  name.length <= PIPELINE_MAX_LENGTH
   return isValid ? [isValid] : [isValid, ERR_PIPELINE_NAME_LENGTH]
 }
 
@@ -58,10 +58,10 @@ function getRepo (github, name) {
   })
 }
 
-function createApp (heroku, {archiveURL, name, organization, pipeline, stage}) {
+function createApp (heroku, { archiveURL, name, organization, pipeline, stage }) {
   const params = {
-    source_blob: {url: archiveURL},
-    app: {name},
+    source_blob: { url: archiveURL },
+    app: { name },
     pipeline_coupling: {
       stage,
       pipeline: pipeline.id
@@ -74,7 +74,7 @@ function createApp (heroku, {archiveURL, name, organization, pipeline, stage}) {
     params.app.personal = true
   }
 
-  return api.createAppSetup(heroku, params).then((setup) => setup)
+  return api.createAppSetup(heroku, params).then((setup) => setup.app)
 }
 
 function* getNameAndRepo (args) {
@@ -146,7 +146,7 @@ function* hasCIFlag (heroku) {
 }
 
 function* getCISettings (yes, organization) {
-  const settings = yes ? {ci: true} : yield prompt([{
+  const settings = yes ? { ci: true } : yield prompt([{
     type: 'confirm',
     name: 'ci',
     message: 'Enable automatic Heroku CI test runs?'
@@ -157,56 +157,6 @@ function* getCISettings (yes, organization) {
   }
 
   return settings
-}
-
-function createApps (heroku, archiveURL, pipeline, pipelineName, stagingAppName, organization) {
-  const prodAppSetupPromise = createApp(heroku, {
-    archiveURL,
-    pipeline,
-    name: pipelineName,
-    stage: 'production',
-    organization
-  })
-
-  const stagingAppSetupPromise = createApp(heroku, {
-    archiveURL,
-    pipeline,
-    name: stagingAppName,
-    stage: 'staging',
-    organization
-  })
-
-  const promises = [prodAppSetupPromise, stagingAppSetupPromise]
-
-  return Promise.all(promises).then(appSetups => {
-    return appSetups
-  }, (error) => {
-    cli.exit(1, error)
-  })
-}
-
-function wait (ms) {
-  return new Promise((resolve, reject) => setTimeout(resolve, ms))
-}
-
-function pollAppSetup (heroku, appSetup) {
-  return api.getAppSetup(heroku, appSetup.id).then((setup) => {
-    if (setup.status === 'succeeded') {
-      return setup
-    }
-
-    if (setup.status === 'failed') {
-      throw new Error(`Couldn't create application ${cli.color.app(setup.app.name)}: ${setup.failure_message}`)
-    }
-
-    return wait(1000).then(() => pollAppSetup(heroku, appSetup))
-  }).catch((error) => {
-    return cli.exit(1, error)
-  })
-}
-
-function pollAppSetups (heroku, appSetups) {
-  return Promise.all(appSetups.map((appSetup) => pollAppSetup(heroku, appSetup)))
 }
 
 function setupPipeline (kolkrabbi, app, settings, pipelineID, ciSettings = {}) {
@@ -231,17 +181,18 @@ module.exports = {
   description: 'bootstrap a new pipeline with common settings and create a production and staging app (requires a fully formed app.json in the repo)',
   help: `Example:
 
-    heroku pipelines:setup example githuborg/reponame -o example-org
-    ? Automatically deploy the master branch to staging? Yes
-    ? Wait for CI to pass before deploying the master branch to staging? Yes
-    ? Enable review apps? Yes
-    ? Automatically create review apps for every PR? Yes
-    ? Automatically destroy idle review apps after 5 days? Yes
-    Creating pipeline... done
-    Linking to repo... done
-    Creating production and staging apps (⬢ example and ⬢ example-staging)
-    Configuring pipeline... done
-    View your new pipeline by running \`heroku pipelines:open e5a55ffa-de3f-11e6-a245-3c15c2e6bc1e\``,
+  heroku pipelines:setup example githuborg/reponame -o example-org
+  ? Automatically deploy the master branch to staging? Yes
+  ? Wait for CI to pass before deploying the master branch to staging? Yes
+  ? Enable review apps? Yes
+  ? Automatically create review apps for every PR? Yes
+  ? Automatically destroy idle review apps after 5 days? Yes
+  Creating pipeline... done
+  Linking to repo... done
+  Creating ⬢ example (production app)... done
+  Creating ⬢ example-staging (staging app)... done
+  Configuring pipeline... done
+  View your new pipeline by running \`heroku pipelines:open e5a55ffa-de3f-11e6-a245-3c15c2e6bc1e\``,
   needsApp: false,
   needsAuth: true,
   args: [
@@ -276,7 +227,7 @@ module.exports = {
       hasValue: false
     }
   ],
-  run: cli.command(co.wrap(function* (context, heroku) {
+  run: cli.command(co.wrap(function*(context, heroku) {
     const errors = validate(context.args)
 
     if (errors.length) {
@@ -288,7 +239,7 @@ module.exports = {
     const github = new GitHubAPI(context.version, yield getGitHubToken(kolkrabbi))
 
     const organization = context.flags.organization || context.flags.team
-    const {name: pipelineName, repo: repoName} = yield getNameAndRepo(context.args)
+    const { name: pipelineName, repo: repoName } = yield getNameAndRepo(context.args)
     const stagingAppName = pipelineName + STAGING_APP_INDICATOR
     const repo = yield getRepo(github, repoName)
     const settings = yield getSettings(context.flags.yes, repo.default_branch)
@@ -309,19 +260,34 @@ module.exports = {
     )
 
     const archiveURL = yield github.getArchiveURL(repoName, repo.default_branch)
-    const appSetups = yield createApps(heroku, archiveURL, pipeline, pipelineName, stagingAppName, organization)
 
     yield cli.action(
-      `Creating production and staging apps (${cli.color.app(pipelineName)} and ${cli.color.app(stagingAppName)})`,
-      pollAppSetups(heroku, appSetups)
+      `Creating ${cli.color.app(pipelineName)} (production app)`,
+      createApp(heroku, {
+        archiveURL,
+        pipeline,
+        name: pipelineName,
+        stage: 'production',
+        organization
+      })
     )
 
-    const stagingApp = appSetups.find((appSetup) => appSetup.app.name === stagingAppName).app
+    const stagingApp = yield cli.action(
+      `Creating ${cli.color.app(stagingAppName)} (staging app)`,
+      createApp(heroku, {
+        archiveURL,
+        pipeline,
+        name: stagingAppName,
+        stage: 'staging',
+        organization
+      })
+    )
 
     yield cli.action(
       'Configuring pipeline',
       setupPipeline(kolkrabbi, stagingApp.id, settings, pipeline.id, ciSettings)
     )
+
     yield cli.open(`https://dashboard.heroku.com/pipelines/${pipeline.id}`)
   }))
 }
